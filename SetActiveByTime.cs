e39ee7fc9423b56b337c4057a763511ba1191b79
Sergey Khanin <khaninsergeyleonidovich@gmail.com>
2022-03-05 21:15:46 +0300

    feat: Arrays supported

--- SetActiveByTime.cs
+++ SetActiveByTime.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class SetActiveByTime : MonoBehaviour
 {
     [SerializeField] private float _delayTime;
-    [SerializeField] private GameObject _setActive;
+    [SerializeField] private GameObject[] _setActive;
     private void Awake()
     {
         StartCoroutine(Appearance());
@@ -11,6 +11,9 @@ public class SetActiveByTime : MonoBehaviour
     IEnumerator Appearance()
     {
         yield return new WaitForSeconds(_delayTime);
-        _setActive.SetActive(!_setActive.activeSelf);
+        for (int i = 0; i < _setActive.Length; i++)
+        {
+            _setActive[i].SetActive(!_setActive[i].activeSelf); 
+        }
     }
 }
